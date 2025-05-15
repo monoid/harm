640b92236bbc921292412614b4cdf920d268e230
Ivan Boldyrev <lispnik@gmail.com>
2025-05-15 08:42:59 +0200

    Download AARCHMRS dataset

--- aarchmrs-gen/src/lib.rs
+++ aarchmrs-gen/src/lib.rs
@@ -3,52 +3,76 @@ use std::path::Path;
 
 use aarchmrs_parser::instructions::{Encodeset, InstructionGroup, InstructionGroupOrInstruction};
 use proc_macro2::TokenStream;
-use stack::{Stack, StackGuard};
 
-pub mod encoding;
-pub mod generation;
-pub mod stack;
+use crate::downloads::{DownloadError, ensure_archive};
+use crate::stack::StackGuard;
+
+mod downloads;
+mod encoding;
+mod generation;
+mod stack;
 
 pub const AARCHMRS_2025_03_URL: &str = "https://developer.arm.com/-/cdn-downloads/permalink/Exploration-Tools-OS-Machine-Readable-Data/AARCHMRS_BSD/AARCHMRS_OPENSOURCE_A_profile-2025-03.tar.gz";
-pub const AARCHMRS_2025_03_MD5: &str = "dcc4850852a18ae0e786ccbe52fafbb0";
+pub const AARCHMRS_2025_03_FILE: &str = "AARCHMRS_OPENSOURCE_A_profile-2025-03.tar.gz";
+pub const AARCHMRS_2025_03_MD5: [u8; 16] = hex_literal::hex!("dcc4850852a18ae0e786ccbe52fafbb0");
 pub const AARCHMRS_2025_03_SIZE: u64 = 3517054;
+pub const AARCHMRS_INSTRUCTIONS_FILE: &str = "Instructions.json";
 
-pub const DATA: &str = include_str!("Instructions.json");
+pub fn gen_data(dest_dir: &Path, cache_dir: &Path) -> Result<(), DownloadError> {
+    let archive_path = ensure_archive(cache_dir)?;
 
-pub fn gen_data(dest_dir: &Path) -> io::Result<()> {
+    let gz_archive_file = std::fs::File::open(archive_path)?;
+    let tar_file = flate2::read::GzDecoder::new(gz_archive_file);
+    let mut archive = tar::Archive::new(tar_file);
+
+    let mut json_data = vec![];
+    for ent in archive.entries()? {
+        let mut ent = ent?;
+        let path = ent.path()?;
+        if path == Path::new(AARCHMRS_INSTRUCTIONS_FILE) {
+            std::io::copy(&mut ent, &mut json_data)?;
+            break;
+        }
+    }
+    assert!(!json_data.is_empty());
+
     let data: aarchmrs_parser::instructions::Instructions =
-        serde_json::from_str(DATA).expect("failed to parse data");
+        serde_json::from_slice(&json_data).expect("failed to parse data");
 
-    let mut stack = Stack::new();
+    let mut encoding_stack = vec![];
 
     let mut lib_mods = vec![];
     for inst_set in &data.instructions {
-        let inst_name = &inst_set.name; // "A64"
+        let inst_name = &inst_set.name; // single entry: "A64"
         let inst_id = quote::format_ident!("{inst_name}");
         lib_mods.push(quote::quote! { pub mod #inst_id; });
 
-        let mut inst_mods = vec![];
+        let mut child_mods = vec![];
 
         let nested_dir = dest_dir.join(inst_name);
         std::fs::create_dir_all(&nested_dir)?;
+
         for inst_group in &inst_set.children {
             // examples: "sve", "sme", "reserved", "dpimm", "control", "ldst", "dpreg", "simd_dp"
             let group_name = &inst_group.name;
             let group_id = quote::format_ident!("{group_name}");
-            inst_mods.push(quote::quote! { pub mod #group_id; });
+            child_mods.push(quote::quote! { pub mod #group_id; });
 
-            let guard = StackGuard::push(&mut stack.data, &inst_group.encoding);
+            let guard = StackGuard::push(&mut encoding_stack, &inst_group.encoding);
             let mod_path = nested_dir.join(format!("{group_name}.rs"));
             let subnested_dir = nested_dir.join(group_name);
-            std::fs::create_dir_all(&subnested_dir)?;
+
             let mods =
                 walk_instructions_children(guard.data, &inst_group.children, &subnested_dir)?;
             write_mod(&mod_path, &mods, &<_>::default())?;
         }
 
         let mod_path = dest_dir.join(format!("{inst_name}.rs"));
-        write_mod(&mod_path, &inst_mods, &<_>::default())?;
+        std::fs::create_dir_all(dest_dir)?;
+        write_mod(&mod_path, &child_mods, &<_>::default())?;
     }
+
+    std::fs::create_dir_all(dest_dir)?;
 
     let lib_path = dest_dir.join("lib.rs");
     let clippy_allow_pragma = quote::quote! { #[allow(
@@ -93,12 +117,14 @@ fn walk_group<'a, 'b: 'a>(
     instruction_group: &'b InstructionGroup,
 ) -> io::Result<TokenStream> {
     let group_name = &instruction_group.name;
-    let group_dir = dest_dir.join(group_name);
     let group_file = dest_dir.join(format!("{group_name}.rs"));
-    std::fs::create_dir_all(&group_dir)?;
+
+    let group_dir = dest_dir.join(group_name);
     let stack2 = StackGuard::push(stack, &instruction_group.encoding);
     let child_mods =
         walk_instructions_children(stack2.data, &instruction_group.children, &group_dir)?;
+
+    std::fs::create_dir_all(dest_dir)?;
     write_mod(&group_file, &child_mods, &<_>::default())?;
 
     let group_id = quote::format_ident!("{group_name}");
