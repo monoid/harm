eb90696866d0895b6c5b8eb58a3a2a6825a65d35
Ivan Boldyrev <lispnik@gmail.com>
2025-05-15 09:36:00 +0200

    (Re-)generate aarchmrs files only if a var is set
    Set `FORCE_AARCHMRS_GEN` to force generation.  Otherwise the generation will be skipped.

--- aarchmrs/build.rs
+++ aarchmrs/build.rs
@@ -3,11 +3,13 @@ use std::path::PathBuf;
 
 fn main() {
     println!("cargo::rerun-if-changed=");
-    let out_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let cache_dir = env::var("OUT_DIR").unwrap();
-    aarchmrs_gen::gen_data(
-        &PathBuf::from(out_dir).join("src"),
-        &PathBuf::from(cache_dir),
-    )
-    .unwrap();
+    if env::var("FORCE_AARCHMRS_GEN").is_ok() {
+        let out_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let cache_dir = env::var("OUT_DIR").unwrap();
+        aarchmrs_gen::gen_data(
+            &PathBuf::from(out_dir).join("src"),
+            &PathBuf::from(cache_dir),
+        )
+        .unwrap();
+    }
 }
