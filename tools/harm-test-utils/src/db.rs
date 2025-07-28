d1c3c1045fb672c0befa59df5e5599ce4d5072cd
Ivan Boldyrev <lispnik@gmail.com>
2025-07-28 05:10:45 +0200

    tools: test tools
    + `harm-test-utils` is a crate with helper code
    + `harm-testgen` is a testcase generator binary

---
+++ tools/harm-test-utils/src/db.rs
@@ -0,0 +1,44 @@
+use aarchmrs_types::InstructionCode;
+use hex::FromHex as _;
+use std::collections::HashMap;
+
+#[derive(Debug, thiserror::Error)]
+pub enum DbError {
+    #[error("Malformed db line: {0}")]
+    MalformedDbLine(&'static str),
+    #[error("Malformed hex opcode: {0}")]
+    MalformedHexOpcode(&'static str),
+}
+
+pub struct Db {
+    codes: HashMap<&'static str, InstructionCode>,
+}
+
+impl Db {
+    pub fn new(db_data: &'static str) -> Result<Self, DbError> {
+        let codes = db_data
+            .split('\n')
+            .filter(|line| !line.is_empty())
+            .map(|line| -> Result<_, DbError> {
+                let (hex, cmd) = line
+                    .split_once('\t')
+                    .ok_or(DbError::MalformedDbLine(line))?;
+                let instruction = InstructionCode::from_hex(hex.as_bytes())
+                    .map_err(|_| DbError::MalformedHexOpcode(hex))?;
+                Ok((cmd, instruction))
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+        let codes = HashMap::from_iter(codes);
+        Ok(Self { codes })
+    }
+
+    pub fn find(&self, key: &str) -> Option<InstructionCode> {
+        self.codes.get(key).cloned()
+    }
+}
+
+pub fn db(db_data: &'static str, key: &str) -> InstructionCode {
+    let db = Db::new(db_data).unwrap_or_else(|e| panic!("{e}"));
+    db.find(key)
+        .unwrap_or_else(|| panic!("key not found: {key:?}"))
+}
---
+++ tools/harm-testgen/src/main.rs
@@ -0,0 +1,154 @@
+use clap::Parser as ClapParser;
+use itertools::Itertools;
+use nom::{
+    IResult, Parser as _,
+    branch::alt,
+    bytes::complete::tag,
+    combinator::{cut, eof, map, opt},
+    multi::separated_list0,
+    sequence::{pair, preceded, terminated},
+};
+
+#[derive(ClapParser)]
+struct Args {
+    pattern: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    for inst in variants(&args.pattern) {
+        println!("\t{inst}");
+    }
+}
+
+fn variants(pattern: &str) -> impl Iterator<Item = String> {
+    nom::combinator::all_consuming(parse)
+        .parse(pattern)
+        .unwrap()
+        .1
+}
+
+struct IterClone<Item>(Box<dyn BoxCloneIter<Item = Item>>);
+
+impl<Item> IterClone<Item> {
+    pub fn new<I: Iterator<Item = Item> + Clone + 'static>(it: I) -> Self {
+        Self(Box::new(it) as _)
+    }
+}
+
+impl<Item> Clone for IterClone<Item> {
+    fn clone(&self) -> Self {
+        Self(self.0.box_clone())
+    }
+}
+
+impl<Item> Iterator for IterClone<Item> {
+    type Item = Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+}
+
+trait BoxCloneIter: Iterator {
+    fn box_clone(&self) -> Box<dyn BoxCloneIter<Item = <Self as Iterator>::Item>>;
+}
+
+impl<T> BoxCloneIter for T
+where
+    T: Iterator + Clone + 'static,
+{
+    fn box_clone(&self) -> Box<dyn BoxCloneIter<Item = <Self as Iterator>::Item>> {
+        Box::new(Clone::clone(self)) as _
+    }
+}
+
+fn parse(inp: &str) -> IResult<&str, IterClone<String>> {
+    alt((
+        map(eof, |_| IterClone::new(std::iter::once(String::new()))),
+        map(pair(parse_item, opt(parse)), |(c, rest)| match rest {
+            Some(rest) => IterClone::new(
+                Itertools::cartesian_product(c, rest).map(|(a, b)| format!("{a}{b}")),
+            ),
+            None => c,
+        }),
+    ))
+    .parse(inp)
+}
+
+fn parse_item(inp: &str) -> IResult<&str, IterClone<String>> {
+    alt((
+        preceded(tag("("), cut(terminated(parse_alt, tag(")")))),
+        map(nom::character::complete::none_of("|)"), |c| {
+            IterClone::new(std::iter::once(format!("{c}")))
+        }),
+    ))
+    .parse(inp)
+}
+
+fn parse_alt(inp: &str) -> IResult<&str, IterClone<String>> {
+    map(separated_list0(tag("|"), parse), |list| {
+        IterClone::new(list.into_iter().flatten())
+    })
+    .parse(inp)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_item_char() {
+        let single = parse_item("a").unwrap().1.collect_vec();
+        assert_eq!(single, &["a"])
+    }
+
+    #[test]
+    fn test_parse_char() {
+        let single = parse("a").unwrap().1.collect_vec();
+        assert_eq!(single, &["a"])
+    }
+
+    #[test]
+    fn test_parse_char_incomplete() {
+        let (rest, single) = parse("a)").unwrap();
+        assert_eq!(rest, ")");
+        assert_eq!(single.collect_vec(), &["a"])
+    }
+
+    #[test]
+    fn test_parse_alt() {
+        let alt = parse_alt("a|b").unwrap().1.collect_vec();
+        assert_eq!(alt, &["a", "b"]);
+    }
+
+    #[test]
+    fn test_empty() {
+        let empty = variants("").collect_vec();
+        assert_eq!(empty, &[""]);
+    }
+
+    #[test]
+    fn test_simple() {
+        let simple = variants("simple").collect_vec();
+        assert_eq!(simple, &["simple"]);
+    }
+
+    #[test]
+    fn test_words() {
+        let var = variants("a b").collect_vec();
+        assert_eq!(var, &["a b"]);
+    }
+
+    #[test]
+    fn test_patterns() {
+        let var = variants("(a|b) (c|d)").collect_vec();
+        assert_eq!(var, &["a c", "a d", "b c", "b d"]);
+    }
+
+    #[test]
+    fn test_patterns_nested() {
+        let var = variants("(a|b(c|d))").collect_vec();
+        assert_eq!(var, &["a", "bc", "bd"]);
+    }
+}
