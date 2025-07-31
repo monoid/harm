034fa43f72a6dd5bf135a84cd3b256f9cdea03cd
Ivan Boldyrev <lispnik@gmail.com>
2025-07-31 18:50:11 +0200

    harm: extended register basic functionality
    Some helper functions and failing variants are not implemented yet.

--- harm/src/instructions/load.rs
+++ harm/src/instructions/load.rs
@@ -18,13 +18,24 @@
 //!
 //! # Examples:
 //! ```ignore
+//! # use harm::instructions::load::{ldr, extended, shifted, LdrExtendOption32, LdrShift};
+//! use harm::register::Reg32::*;
+//! use harm::register::Reg64::*;
+//! use LdrExtendOption32::*;
 //! ldr(W1, X2);        // LDR W1, [X2]
 //! ldr(W1, (X2,));     // LDR W1, [X2]
 //! ldr(W1, (X2, X3));  // LDR W1, [X2, X3] ; n.b. a 32-bit register offset requires extend speicifer (sxtw or uxtw):
-//! ldr(X1, (X2, uxtw(W3, 2))).unwrap();  // LDR X1, [X2, W3 uxtw 3]
-//! ldr(X1, (X2, sxtw(W3, 2))).unwrap();  // LDR X1, [X2, W3 sxtw 3]
-//! ldr(X1, (X2, uxtw(W3, 2).unwrap()));  // LDR X1, [X2, W3 uxtw 3]
-//! ldr(X1, (X2, sxtw(W3, 2).unwrap()));  // LDR X1, [X2, W3 sxtw 3]
+//! ldr(W1, (X2, extended(W3, UXTW))); // ldr w1, [x2, w3, uxtw]
+//! ldr(W1, (X2, extended(shifted_by(W3, LdrShift::Shifted), UXTW))); // ldr w1, [x2, w3, uxtw #2]
+//! ldr(W1, (X2, extended(W3, UXTW))); // ldr w1, [x2, w3, uxtw]
+//! ldr(X1, (X2, extended(W3, UXTW))); // ldr x1, [x2, w3, uxtw]
+//! ldr(X1, (X2, extended(shifted_by(W3, LdrShift::Shifted), UXTW))); // ldr x1, [x2, w3, uxtw #3]
+//! # // TODO ldr(X1, (X2, uxtw(W3, 2))).unwrap();  // LDR X1, [X2, W3 uxtw 3]
+//! # // TODO ldr(X1, (X2, sxtw(W3, 2))).unwrap();  // LDR X1, [X2, W3 sxtw 3]
+//! # // TODO ldr(X1, (X2, uxtw(W3, 2).unwrap()));  // LDR X1, [X2, W3 uxtw 3]
+//! # // TODO ldr(X1, (X2, sxtw(W3, 2).unwrap()));  // LDR X1, [X2, W3 sxtw 3]
+//! # // TODO ldr(X1, (X2, extended((W3, 3)))).unwrap(); // ldr x1, [x2, w3, uxtw 3]
+//! # // TODO uxtw/sxtw functions
 //! ```
 //!
 //! Please note, that `uxtw` and `sxtw` can be used only with 32-bit register, and shift can be only either 0 or 2. The
@@ -90,21 +101,147 @@ use aarchmrs_instructions::A64::ldst::{
     ldst_pos::{LDR_32_ldst_pos::LDR_32_ldst_pos, LDR_64_ldst_pos::LDR_64_ldst_pos},
     ldst_regoff::{LDR_32_ldst_regoff::LDR_32_ldst_regoff, LDR_64_ldst_regoff::LDR_64_ldst_regoff},
 };
+use aarchmrs_types::BitValue;
 
 use super::Instruction;
 use crate::{
     bits::{BitError, SBitValue, UBitValue},
-    register::{IntoCode, Reg32, Reg64, RegOrSp64, RegOrZero64},
+    register::{IntoCode, Reg32, Reg64, RegOrSp64, RegOrZero32, RegOrZero64},
 };
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Default)]
 #[repr(u8)]
+// It seems that these variants are actually noop: you cannot actually extend r64 to r64.
+// It is only useful if you want to produce specific bit pattern.
 pub enum LdrExtendOption64 {
     #[default]
     LSL = 0b011,
-    // It seems that these variants are noop: you cannot actually extend r64 to r64.
-    // It is only useful if you want to produce specific bit pattern.
     SXTX = 0b111,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Default)]
+#[repr(u8)]
+pub enum LdrExtendOption32 {
+    #[default]
+    UXTW = 0b010,
+    SXTW = 0b110,
+}
+
+pub trait LdrDestShiftOption {
+    const SHIFT_SIZE: u32;
+}
+pub trait LdrOffsetExtendOption {
+    type ExtendOption;
+}
+
+impl LdrDestShiftOption for Reg64 {
+    const SHIFT_SIZE: u32 = 3;
+}
+
+impl LdrOffsetExtendOption for Reg64 {
+    type ExtendOption = LdrExtendOption64;
+}
+
+impl LdrDestShiftOption for RegOrZero64 {
+    const SHIFT_SIZE: u32 = 3;
+}
+
+impl LdrOffsetExtendOption for RegOrZero64 {
+    type ExtendOption = LdrExtendOption64;
+}
+
+impl LdrDestShiftOption for Reg32 {
+    const SHIFT_SIZE: u32 = 2;
+}
+
+impl LdrOffsetExtendOption for Reg32 {
+    type ExtendOption = LdrExtendOption32;
+}
+
+impl LdrDestShiftOption for RegOrZero32 {
+    const SHIFT_SIZE: u32 = 2;
+}
+
+impl LdrOffsetExtendOption for RegOrZero32 {
+    type ExtendOption = LdrExtendOption32;
+}
+
+pub struct Extended<Dest: LdrDestShiftOption, Offset: LdrOffsetExtendOption> {
+    pub shifted: Shifted<Dest, Offset>,
+    pub extend: <Offset as LdrOffsetExtendOption>::ExtendOption,
+}
+
+pub fn shifted<Dest: LdrDestShiftOption, Offset: LdrOffsetExtendOption>(
+    offset: Offset,
+    shifted: LdrShift,
+) -> Shifted<Dest, Offset> {
+    Shifted {
+        offset,
+        shifted,
+        phantom: std::marker::PhantomData,
+    }
+}
+
+impl<Dest, Offset> From<Offset> for Shifted<Dest, Offset> {
+    fn from(reg: Offset) -> Self {
+        Shifted {
+            offset: reg,
+            shifted: LdrShift::Unshifted,
+            phantom: std::marker::PhantomData,
+        }
+    }
+}
+
+pub fn extended<Dest, Offset>(
+    shifted: impl Into<Shifted<Dest, Offset>>,
+    extend: <Offset as LdrOffsetExtendOption>::ExtendOption,
+) -> Extended<Dest, Offset>
+where
+    Dest: LdrDestShiftOption,
+    Offset: LdrOffsetExtendOption,
+{
+    Extended {
+        shifted: shifted.into(),
+        extend: extend,
+    }
+}
+
+pub fn shifted_by<Dest: LdrDestShiftOption, Offset: LdrOffsetExtendOption>(
+    reg: Offset,
+    shift: u32,
+) -> Result<Shifted<Dest, Offset>, ShiftedError> {
+    let accepted = Dest::SHIFT_SIZE;
+    let shifted = if shift == 0 {
+        LdrShift::Unshifted
+    } else if shift == accepted {
+        LdrShift::Shifted
+    } else {
+        return Err(ShiftedError::InvalidShiftSize { shift, accepted });
+    };
+    Ok(Shifted {
+        offset: reg,
+        shifted,
+        phantom: std::marker::PhantomData,
+    })
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ShiftedError {
+    InvalidShiftSize { shift: u32, accepted: u32 },
+}
+
+use std::fmt;
+impl fmt::Display for ShiftedError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InvalidShiftSize { shift, accepted } => {
+                write!(
+                    f,
+                    "Invalid shift {shift} for a register of shift size {accepted}"
+                )
+            }
+        }
+    }
 }
 
 pub type PcOffset = SBitValue<19>;
@@ -123,14 +260,21 @@ pub const fn unscaled(base: RegOrSp64, offset: UnscaledOffset) -> Unscaled {
 }
 
 #[repr(u8)]
-pub enum RegShift {
+pub enum LdrShift {
     Unshifted = 0,
     Shifted = 1,
 }
 
-pub struct Shifted<Reg> {
-    pub reg: Reg,
-    pub shifted: RegShift,
+impl From<LdrShift> for BitValue<1> {
+    fn from(value: LdrShift) -> Self {
+        (value as u8).into()
+    }
+}
+
+pub struct Shifted<Dest, Reg> {
+    pub offset: Reg,
+    pub shifted: LdrShift,
+    phantom: std::marker::PhantomData<Dest>,
 }
 
 pub struct Load<Dst, Base, Offset> {
@@ -144,15 +288,139 @@ pub trait MakeLoad<Dst, Base, Offset> {
     fn new(dst: Dst, base: Base, offset: Offset) -> Self::Output;
 }
 
-impl<B, O> MakeLoad<Reg64, B, O> for Load<Reg64, RegOrSp64, RegOrZero64>
+impl<Base, Ext> MakeLoad<Reg64, Base, Ext> for Load<Reg64, RegOrSp64, Extended<Reg64, Reg64>>
 where
-    B: Into<RegOrSp64>,
-    O: Into<RegOrZero64>,
+    Base: Into<RegOrSp64>,
+    Ext: Into<Extended<Reg64, Reg64>>,
 {
     type Output = Self;
 
     #[inline]
-    fn new(dst: Reg64, base: B, offset: O) -> Self {
+    fn new(dst: Reg64, base: Base, offset: Ext) -> Self {
+        Self {
+            dst,
+            base: base.into(),
+            offset: offset.into(),
+        }
+    }
+}
+
+impl Instruction for Load<Reg64, RegOrSp64, Extended<Reg64, Reg64>> {
+    #[inline]
+    fn represent(self) -> impl Iterator<Item = aarchmrs_types::InstructionCode> + 'static {
+        let code = LDR_64_ldst_regoff(
+            self.offset.shifted.offset.code(),
+            (self.offset.extend as u8).into(),
+            self.offset.shifted.shifted.into(),
+            self.base.code(),
+            self.dst.code(),
+        );
+        std::iter::once(code)
+    }
+}
+
+impl<Base, Ext> MakeLoad<Reg64, Base, Ext> for Load<Reg64, RegOrSp64, Extended<Reg64, Reg32>>
+where
+    Base: Into<RegOrSp64>,
+    Ext: Into<Extended<Reg64, Reg32>>,
+{
+    type Output = Self;
+
+    #[inline]
+    fn new(dst: Reg64, base: Base, offset: Ext) -> Self {
+        Self {
+            dst,
+            base: base.into(),
+            offset: offset.into(),
+        }
+    }
+}
+
+impl Instruction for Load<Reg64, RegOrSp64, Extended<Reg64, Reg32>> {
+    #[inline]
+    fn represent(self) -> impl Iterator<Item = aarchmrs_types::InstructionCode> + 'static {
+        let code = LDR_64_ldst_regoff(
+            self.offset.shifted.offset.code(),
+            (self.offset.extend as u8).into(),
+            self.offset.shifted.shifted.into(),
+            self.base.code(),
+            self.dst.code(),
+        );
+        std::iter::once(code)
+    }
+}
+
+impl<Base, Ext> MakeLoad<Reg32, Base, Ext> for Load<Reg32, RegOrSp64, Extended<Reg32, Reg64>>
+where
+    Base: Into<RegOrSp64>,
+    Ext: Into<Extended<Reg32, Reg64>>,
+{
+    type Output = Self;
+
+    #[inline]
+    fn new(dst: Reg32, base: Base, offset: Ext) -> Self {
+        Self {
+            dst,
+            base: base.into(),
+            offset: offset.into(),
+        }
+    }
+}
+
+impl Instruction for Load<Reg32, RegOrSp64, Extended<Reg32, Reg64>> {
+    #[inline]
+    fn represent(self) -> impl Iterator<Item = aarchmrs_types::InstructionCode> + 'static {
+        let code = LDR_64_ldst_regoff(
+            self.offset.shifted.offset.code(),
+            (self.offset.extend as u8).into(),
+            self.offset.shifted.shifted.into(),
+            self.base.code(),
+            self.dst.code(),
+        );
+        std::iter::once(code)
+    }
+}
+
+impl<Base, Ext> MakeLoad<Reg32, Base, Ext> for Load<Reg32, RegOrSp64, Extended<Reg32, Reg32>>
+where
+    Base: Into<RegOrSp64>,
+    Ext: Into<Extended<Reg32, Reg32>>,
+{
+    type Output = Self;
+
+    #[inline]
+    fn new(dst: Reg32, base: Base, offset: Ext) -> Self {
+        Self {
+            dst,
+            base: base.into(),
+            offset: offset.into(),
+        }
+    }
+}
+
+impl Instruction for Load<Reg32, RegOrSp64, Extended<Reg32, Reg32>> {
+    #[inline]
+    fn represent(self) -> impl Iterator<Item = aarchmrs_types::InstructionCode> + 'static {
+        let code = LDR_64_ldst_regoff(
+            self.offset.shifted.offset.code(),
+            (self.offset.extend as u8).into(),
+            self.offset.shifted.shifted.into(),
+            self.base.code(),
+            self.dst.code(),
+        );
+        std::iter::once(code)
+    }
+}
+
+impl<Base, OffsetReg> MakeLoad<Reg64, Base, OffsetReg> for Load<Reg64, RegOrSp64, RegOrZero64>
+where
+    Base: Into<RegOrSp64>,
+    OffsetReg: Into<RegOrZero64>,
+{
+    type Output = Self;
+
+    #[inline]
+    fn new(dst: Reg64, base: Base, offset: OffsetReg) -> Self {
         Self {
             dst,
             base: base.into(),
@@ -319,6 +587,32 @@ f8636be2	ldr x2, [sp, x3]
 f87f6be2	ldr x2, [sp, xzr]
 ";
 
+    const SIMPLE_LDR_REG_EXT_DB: &str = "
+f8634902	ldr x2, [x8, w3, uxtw]
+f8634902	ldr x2, [x8, w3, uxtw #0]
+f8635902	ldr x2, [x8, w3, uxtw #3]
+f863c902	ldr x2, [x8, w3, sxtw]
+f863c902	ldr x2, [x8, w3, sxtw #0]
+f863d902	ldr x2, [x8, w3, sxtw #3]
+f863e902	ldr x2, [x8, x3, sxtx]
+f863e902	ldr x2, [x8, x3, sxtx #0]
+f863f902	ldr x2, [x8, x3, sxtx #3]
+f8636902	ldr x2, [x8, x3]
+f8636902	ldr x2, [x8, x3, lsl #0]
+f8637902	ldr x2, [x8, x3, lsl #3]
+b8634902	ldr w2, [x8, w3, uxtw]
+b8634902	ldr w2, [x8, w3, uxtw #0]
+b8635902	ldr w2, [x8, w3, uxtw #2]
+b863c902	ldr w2, [x8, w3, sxtw]
+b863c902	ldr w2, [x8, w3, sxtw #0]
+b863d902	ldr w2, [x8, w3, sxtw #2]
+b863e902	ldr w2, [x8, x3, sxtx]
+b863e902	ldr w2, [x8, x3, sxtx #0]
+b863f902	ldr w2, [x8, x3, sxtx #2]
+b8636902	ldr w2, [x8, x3]
+b8636902	ldr w2, [x8, x3, lsl #0]
+b8637902	ldr w2, [x8, x3, lsl #2]";
+
     // 'ldr (w2|x2), [(x8|sp), #0x190]'
     const SIMPLE_LDR_SCALED_IMM: &str = "
 b9419102	ldr w2, [x8, #0x190]
@@ -326,6 +620,46 @@ b94193e2	ldr w2, [sp, #0x190]
 f940c902	ldr x2, [x8, #0x190]
 f940cbe2	ldr x2, [sp, #0x190]
 ";
+
+    #[test]
+    fn test_ldr_r64_r64_r32_sxtw() {
+        use LdrExtendOption32::*;
+        let inst: Vec<_> = ldr(X2, (X8, extended(W3, SXTW))).represent().collect();
+        assert_eq!(
+            inst,
+            vec![db(SIMPLE_LDR_REG_EXT_DB, "ldr x2, [x8, w3, sxtw]")]
+        );
+    }
+
+    #[test]
+    fn test_ldr_r64_r64_r32_uxtw() {
+        use LdrExtendOption32::*;
+        let inst: Vec<_> = ldr(X2, (X8, extended(W3, UXTW))).represent().collect();
+        assert_eq!(
+            inst,
+            vec![db(SIMPLE_LDR_REG_EXT_DB, "ldr x2, [x8, w3, uxtw]")]
+        );
+    }
+
+    #[test]
+    fn test_ldr_r32_r64_r32_sxtw() {
+        use LdrExtendOption32::*;
+        let inst: Vec<_> = ldr(W2, (X8, extended(W3, SXTW))).represent().collect();
+        assert_eq!(
+            inst,
+            vec![db(SIMPLE_LDR_REG_EXT_DB, "ldr x2, [x8, w3, sxtw]")]
+        );
+    }
+
+    #[test]
+    fn test_ldr_r32_r64_r32_uxtw() {
+        use LdrExtendOption32::*;
+        let inst: Vec<_> = ldr(W2, (X8, extended(W3, UXTW))).represent().collect();
+        assert_eq!(
+            inst,
+            vec![db(SIMPLE_LDR_REG_EXT_DB, "ldr x2, [x8, w3, uxtw]")]
+        );
+    }
 
     #[test]
     fn test_ldr_r32_r64_r64() {
