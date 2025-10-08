d923e2fdd56a0b65243552293ec8205990554e84
Ivan Boldyrev <lispnik@gmail.com>
2025-10-08 21:02:47 +0200

    harm: implement `tst`

--- harm/src/instructions/dpimm/log_imm/args.rs
+++ harm/src/instructions/dpimm/log_imm/args.rs
@@ -40,7 +40,8 @@ where
     }
 }
 
-impl<RdIn, RnIn> MakeSpLogicalArgs<RdIn, RnIn, u32> for LogicalArgs<RegOrSp32, RegOrZero32, LogicalImmFields>
+impl<RdIn, RnIn> MakeSpLogicalArgs<RdIn, RnIn, u32>
+    for LogicalArgs<RegOrSp32, RegOrZero32, LogicalImmFields>
 where
     RdIn: IntoReg<RegOrSp32>,
     RnIn: IntoReg<RegOrZero32>,
@@ -73,7 +74,8 @@ where
     }
 }
 
-impl<RdIn, RnIn> MakeSpLogicalArgs<RdIn, RnIn, u64> for LogicalArgs<RegOrSp64, RegOrZero64, LogicalImmFields>
+impl<RdIn, RnIn> MakeSpLogicalArgs<RdIn, RnIn, u64>
+    for LogicalArgs<RegOrSp64, RegOrZero64, LogicalImmFields>
 where
     RdIn: IntoReg<RegOrSp64>,
     RnIn: IntoReg<RegOrZero64>,
@@ -112,7 +114,8 @@ where
     }
 }
 
-impl<RdIn, RnIn> MakeZeroLogicalArgs<RdIn, RnIn, u32> for LogicalArgs<RegOrZero32, RegOrZero32, LogicalImmFields>
+impl<RdIn, RnIn> MakeZeroLogicalArgs<RdIn, RnIn, u32>
+    for LogicalArgs<RegOrZero32, RegOrZero32, LogicalImmFields>
 where
     RdIn: IntoReg<RegOrZero32>,
     RnIn: IntoReg<RegOrZero32>,
@@ -145,7 +148,8 @@ where
     }
 }
 
-impl<RdIn, RnIn> MakeZeroLogicalArgs<RdIn, RnIn, u64> for LogicalArgs<RegOrZero64, RegOrZero64, LogicalImmFields>
+impl<RdIn, RnIn> MakeZeroLogicalArgs<RdIn, RnIn, u64>
+    for LogicalArgs<RegOrZero64, RegOrZero64, LogicalImmFields>
 where
     RdIn: IntoReg<RegOrZero64>,
     RnIn: IntoReg<RegOrZero64>,
@@ -155,6 +159,74 @@ where
     fn new(rd: RdIn, rn: RnIn, mask: u64) -> Self::Outcome {
         LogicalImmFields::try_new64(mask).map(|mask| Self {
             rd: rd.into_reg(),
+            rn: rn.into_reg(),
+            mask,
+        })
+    }
+}
+
+pub trait MakeTstLogicalArgs<Rn, Mask> {
+    type Outcome: Outcome;
+
+    fn new(rn: Rn, mask: Mask) -> Self::Outcome;
+}
+
+impl<RnIn> MakeTstLogicalArgs<RnIn, LogicalImmFields>
+    for LogicalArgs<RegOrZero32, RegOrZero32, LogicalImmFields>
+where
+    RnIn: IntoReg<RegOrZero32>,
+{
+    type Outcome = Unfallible<Self>;
+
+    fn new(rn: RnIn, mask: LogicalImmFields) -> Self::Outcome {
+        Unfallible(Self {
+            rd: RegOrZero32::WZR,
+            rn: rn.into_reg(),
+            mask,
+        })
+    }
+}
+
+impl<RnIn> MakeTstLogicalArgs<RnIn, u32> for LogicalArgs<RegOrZero32, RegOrZero32, LogicalImmFields>
+where
+    RnIn: IntoReg<RegOrZero32>,
+{
+    type Outcome = Result<Self, LogicalImmError>;
+
+    fn new(rn: RnIn, mask: u32) -> Self::Outcome {
+        LogicalImmFields::try_new32(mask).map(|mask| Self {
+            rd: RegOrZero32::WZR,
+            rn: rn.into_reg(),
+            mask,
+        })
+    }
+}
+
+impl<RnIn> MakeTstLogicalArgs<RnIn, LogicalImmFields>
+    for LogicalArgs<RegOrZero64, RegOrZero64, LogicalImmFields>
+where
+    RnIn: IntoReg<RegOrZero64>,
+{
+    type Outcome = Unfallible<Self>;
+
+    fn new(rn: RnIn, mask: LogicalImmFields) -> Self::Outcome {
+        Unfallible(Self {
+            rd: RegOrZero64::XZR,
+            rn: rn.into_reg(),
+            mask,
+        })
+    }
+}
+
+impl<RnIn> MakeTstLogicalArgs<RnIn, u64> for LogicalArgs<RegOrZero64, RegOrZero64, LogicalImmFields>
+where
+    RnIn: IntoReg<RegOrZero64>,
+{
+    type Outcome = Result<Self, LogicalImmError>;
+
+    fn new(rn: RnIn, mask: u64) -> Self::Outcome {
+        LogicalImmFields::try_new64(mask).map(|mask| Self {
+            rd: RegOrZero64::XZR,
             rn: rn.into_reg(),
             mask,
         })
