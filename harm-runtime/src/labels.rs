0bbae7c892eb08c5640662eff6d641b312111482
Ivan Boldyrev <lispnik@gmail.com>
2025-12-29 13:34:53 +0100

    runtime: Remove unfinished code

--- harm-runtime/src/labels.rs
+++ harm-runtime/src/labels.rs
@@ -3,93 +3,11 @@
  * This document is licensed under the BSD 3-clause license.
  */
 
-use std::collections::HashMap;
-
-use harm::reloc::{LabelId, Offset};
+use harm::reloc::Offset;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum LabelInfo {
     Forward,
     // TODO segment
     Offset(Offset),
-}
-
-#[derive(Debug, Default)]
-pub struct LabelRegistry {
-    named_labels: HashMap<String, LabelId>,
-    labels: HashMap<LabelId, LabelInfo>,
-    next_id: usize,
-}
-
-impl LabelRegistry {
-    #[inline]
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    #[inline]
-    pub fn forward_named_label(&mut self, name: &str) -> LabelId {
-        if let Some(id) = self.named_labels.get(name) {
-            *id
-        } else {
-            let id = self.next_label();
-            self.named_labels.insert(name.to_string(), id);
-            self.labels.insert(id, LabelInfo::Forward);
-            id
-        }
-    }
-
-    #[inline]
-    pub fn forward_label(&mut self) -> LabelId {
-        let id = self.next_label();
-        self.labels.insert(id, LabelInfo::Forward);
-        id
-    }
-
-    pub fn define_label(&mut self, id: LabelId, offset: Offset) {
-        if let Some(info) = self.labels.get_mut(&id) {
-            match info {
-                LabelInfo::Forward => {
-                    *info = LabelInfo::Offset(offset);
-                }
-                LabelInfo::Offset(_) => {
-                    todo!("Label {id:?} is already defined");
-                }
-            }
-        } else {
-            panic!("Label {id:?} is not registered");
-        }
-    }
-
-    #[inline]
-    pub fn define_named_label(&mut self, name: &str, offset: Offset) -> LabelId {
-        if let Some(id) = self.named_labels.get(name).copied() {
-            self.labels.insert(id, LabelInfo::Offset(offset));
-            id
-        } else {
-            let id = self.next_label();
-            self.named_labels.insert(name.to_string(), id);
-            self.labels.insert(id, LabelInfo::Offset(offset));
-            id
-        }
-    }
-
-    pub fn name_label(&mut self, id: LabelId, name: &str) {
-        if self.labels.contains_key(&id) {
-            self.named_labels.insert(name.to_string(), id);
-        } else {
-            panic!("Label {id:?} is not registered");
-        }
-    }
-
-    #[inline]
-    pub fn label_info(&self, id: LabelId) -> Option<&LabelInfo> {
-        self.labels.get(&id)
-    }
-
-    fn next_label(&mut self) -> LabelId {
-        let id = LabelId(self.next_id);
-        self.next_id += 1;
-        id
-    }
 }
--- harm-runtime/src/lib.rs
+++ harm-runtime/src/lib.rs
@@ -4,4 +4,3 @@
  */
 
 pub mod labels;
-pub mod runtime;
