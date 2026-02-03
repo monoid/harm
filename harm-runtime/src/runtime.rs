bff64be6af457c8c485c600013c770dabd2f6047
Ivan Boldyrev <lispnik@gmail.com>
2026-02-03 22:24:04 +0100

    runtime: Assembler::build

--- harm-runtime/src/runtime.rs
+++ harm-runtime/src/runtime.rs
@@ -5,52 +5,59 @@
 
 use std::collections::HashMap;
 
+use crate::builder::Builder;
 use crate::labels::LabelRegistry;
+use crate::memory::{FixedMemory, Memory};
 use harm::instructions::InstructionSeq;
 use harm::reloc::{LabelId, Offset64, Rel64};
 
-// N.N. we keep here internal relocation type, and convert it to external on serialization.
+// N.B. we keep here internal relocation type, and convert it to external on serialization.
 #[derive(Default)]
-pub struct Assembler {
+pub struct Assembler<FM: FixedMemory, Mem: Memory<FM>> {
     label_manager: LabelRegistry,
-    memory: Vec<u8>,
+    memory: Mem,
     relocations: HashMap<usize, Rel64>,
+    phantom: core::marker::PhantomData<FM>,
 }
 
-impl Assembler {
+impl<FM: FixedMemory, Mem: Memory<FM>> Assembler<FM, Mem> {
     #[inline]
-    pub fn new() -> Self {
-        <_>::default()
-    }
-
-    pub fn build(self) {
-        todo!()
-    }
-
-    #[inline]
-    pub fn with_capacity(cap: usize) -> Self {
+    pub fn new(mem: Mem) -> Self {
         Self {
             label_manager: LabelRegistry::new(),
-            memory: Vec::with_capacity(cap),
+            memory: mem,
             relocations: HashMap::new(),
+            phantom: core::marker::PhantomData,
         }
     }
 
-    pub fn insert<InstSeq: InstructionSeq>(&mut self, s: InstSeq) {
+    pub fn build(self) -> Result<FM::ExecutableMemory, BuilderError> {
+        let mut fixed_memory = self.memory.into_fixed_memory()?;
+        let base = fixed_memory.as_mut().as_ptr() as u64;
+        let builder = Builder::new(fixed_memory.as_mut(), base);
+        builder.build(
+            self.label_manager.defined_labels().map(|(name, offset)| (name, offset as i64)),
+            self.relocations.into_iter(),
+        )?;
+        fixed_memory.into_executable_memory()
+    }
+
+    pub fn append<InstSeq: InstructionSeq>(&mut self, s: InstSeq) -> Result<(), Mem::ExtendError> {
         // TODO align by instruction alignment?
         for (inst, rel) in s.encode() {
-            let pos = self.memory.len();
-            self.memory.extend(inst.0);
+            let pos = self.memory.pos();
+            self.memory.try_extend(inst.0.iter().cloned())?;
             if let Some(rel) = rel {
                 self.relocations.insert(pos, rel);
             }
         }
+        Ok(())
     }
 
     // TODO the label have to be aligned.  Except for data labels?..
     // For an instruction, it is alwasy 4 bytes, but for data it can be different, from 1 to N bytes.
     pub fn current_label(&mut self) -> LabelId {
-        let pos = self.memory.len();
+        let pos = self.memory.pos();
 
         // TODO can be fused
         let label_id = self.label_manager.forward_label();
@@ -74,7 +81,7 @@ impl Assembler {
     }
 
     pub fn assign_forward_label(&mut self, label_id: LabelId) {
-        let pos = self.memory.len();
+        let pos = self.memory.pos();
 
         self.label_manager.define_label(label_id, pos as Offset64);
     }
