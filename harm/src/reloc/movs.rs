e549c5db9e136e5b0f87882448a7e69d279e6461
Ivan Boldyrev <lispnik@gmail.com>
2026-03-14 18:06:54 +0100

    Misc

--- harm/src/reloc/movs.rs
+++ harm/src/reloc/movs.rs
@@ -17,137 +17,87 @@ const MOV_IMM16_OFFSET: u32 = 5;
 const MOV_IMM16_WIDTH: u32 = 16;
 
 pub fn mov_w_abs_g0_reloc(value: u64, mem: &mut [u8], offset: usize) -> Result<(), Rel64Error> {
-    const INST_MASK: u32 = ((1 << MOV_IMM16_WIDTH) - 1) << MOV_IMM16_OFFSET;
-
     let target: u16 = value.try_into()?;
     let bytes = get_bytes_mut(mem, offset)?;
-
-    let mut inst_code = InstructionCode(*bytes).unpack();
-    inst_code &= !INST_MASK;
-    inst_code |= (target as u32) << MOV_IMM16_OFFSET;
-    *bytes = InstructionCode::from_u32(inst_code).0;
-
+    patch_mov(target, bytes);
     Ok(())
 }
 
 pub fn mov_w_abs_g0nc_reloc(value: u64, mem: &mut [u8], offset: usize) -> Result<(), Rel64Error> {
-    const INST_MASK: u32 = ((1 << MOV_IMM16_WIDTH) - 1) << MOV_IMM16_OFFSET;
-
     let target = value as u16;
     let bytes = get_bytes_mut(mem, offset)?;
-
-    let mut inst_code = InstructionCode(*bytes).unpack();
-    inst_code &= !INST_MASK;
-    inst_code |= (target as u32) << MOV_IMM16_OFFSET;
-    *bytes = InstructionCode::from_u32(inst_code).0;
-
+    patch_mov(target, bytes);
     Ok(())
 }
 
 pub fn mov_w_abs_g0s_reloc(value: i64, mem: &mut [u8], offset: usize) -> Result<(), Rel64Error> {
-    const INST_MASK: u32 = (((1 << MOV_IMM16_WIDTH) - 1) << MOV_IMM16_OFFSET)
-        | (((1 << MOV_OPCODE_WIDTH) - 1) << MOV_OPCODE_OFFSET);
-
     let target: i16 = value.try_into()?;
     let bytes = get_bytes_mut(mem, offset)?;
-
-    let mut inst_code = InstructionCode(*bytes).unpack();
-    inst_code &= !INST_MASK;
-    if target < 0 {
-        inst_code |= ((!target as u16) as u32) << MOV_IMM16_OFFSET;
-        inst_code |= MOV_OPCODE_MOVN << MOV_OPCODE_OFFSET;
-    } else {
-        inst_code |= ((target as u16) as u32) << MOV_IMM16_OFFSET;
-        inst_code |= MOV_OPCODE_MOVZ << MOV_OPCODE_OFFSET;
-    }
-    *bytes = InstructionCode::from_u32(inst_code).0;
-
+    patch_mov_signed(target, bytes);
     Ok(())
 }
 
 pub fn mov_w_abs_g1_reloc(value: u64, mem: &mut [u8], offset: usize) -> Result<(), Rel64Error> {
-    const INST_MASK: u32 = ((1 << MOV_IMM16_WIDTH) - 1) << MOV_IMM16_OFFSET;
-
     let target: u16 = (value >> 16).try_into()?;
     let bytes = get_bytes_mut(mem, offset)?;
-
-    let mut inst_code = InstructionCode(*bytes).unpack();
-    inst_code &= !INST_MASK;
-    inst_code |= (target as u32) << MOV_IMM16_OFFSET;
-    *bytes = InstructionCode::from_u32(inst_code).0;
-
+    patch_mov(target, bytes);
     Ok(())
 }
 
 pub fn mov_w_abs_g1nc_reloc(value: u64, mem: &mut [u8], offset: usize) -> Result<(), Rel64Error> {
-    const INST_MASK: u32 = ((1 << MOV_IMM16_WIDTH) - 1) << MOV_IMM16_OFFSET;
-
     let target = (value >> 16) as u16;
     let bytes = get_bytes_mut(mem, offset)?;
-
-    let mut inst_code = InstructionCode(*bytes).unpack();
-    inst_code &= !INST_MASK;
-    inst_code |= (target as u32) << MOV_IMM16_OFFSET;
-    *bytes = InstructionCode::from_u32(inst_code).0;
-
+    patch_mov(target, bytes);
     Ok(())
 }
 
 pub fn mov_w_abs_g1s_reloc(value: i64, mem: &mut [u8], offset: usize) -> Result<(), Rel64Error> {
-    const INST_MASK: u32 = (((1 << MOV_IMM16_WIDTH) - 1) << MOV_IMM16_OFFSET)
-        | (((1 << MOV_OPCODE_WIDTH) - 1) << MOV_OPCODE_OFFSET);
-
     let target: i16 = (value >> 16).try_into()?;
     let bytes = get_bytes_mut(mem, offset)?;
-
-    let mut inst_code = InstructionCode(*bytes).unpack();
-    inst_code &= !INST_MASK;
-    if target < 0 {
-        inst_code |= ((!target as u16) as u32) << MOV_IMM16_OFFSET;
-        inst_code |= MOV_OPCODE_MOVN << MOV_OPCODE_OFFSET;
-    } else {
-        inst_code |= ((target as u16) as u32) << MOV_IMM16_OFFSET;
-        inst_code |= MOV_OPCODE_MOVZ << MOV_OPCODE_OFFSET;
-    }
-    *bytes = InstructionCode::from_u32(inst_code).0;
-
+    patch_mov_signed(target, bytes);
     Ok(())
 }
 
 pub fn mov_w_abs_g2_reloc(value: u64, mem: &mut [u8], offset: usize) -> Result<(), Rel64Error> {
-    const INST_MASK: u32 = ((1 << MOV_IMM16_WIDTH) - 1) << MOV_IMM16_OFFSET;
-
     let target: u16 = (value >> 32).try_into()?;
     let bytes = get_bytes_mut(mem, offset)?;
-
-    let mut inst_code = InstructionCode(*bytes).unpack();
-    inst_code &= !INST_MASK;
-    inst_code |= (target as u32) << MOV_IMM16_OFFSET;
-    *bytes = InstructionCode::from_u32(inst_code).0;
-
+    patch_mov(target, bytes);
     Ok(())
 }
 
 pub fn mov_w_abs_g2nc_reloc(value: u64, mem: &mut [u8], offset: usize) -> Result<(), Rel64Error> {
-    const INST_MASK: u32 = ((1 << MOV_IMM16_WIDTH) - 1) << MOV_IMM16_OFFSET;
-
     let target = (value >> 32) as u16;
     let bytes = get_bytes_mut(mem, offset)?;
+    patch_mov(target, bytes);
+    Ok(())
+}
+
+pub fn mov_w_abs_g2s_reloc(value: i64, mem: &mut [u8], offset: usize) -> Result<(), Rel64Error> {
+    let target: i16 = (value >> 32).try_into()?;
+    let bytes = get_bytes_mut(mem, offset)?;
+    patch_mov_signed(target, bytes);
+    Ok(())
+}
+
+pub fn mov_w_abs_g3_reloc(value: u64, mem: &mut [u8], offset: usize) -> Result<(), Rel64Error> {
+    let target = (value >> 48) as u16;
+    let bytes = get_bytes_mut(mem, offset)?;
+    patch_mov(target, bytes);
+    Ok(())
+}
+
+fn patch_mov(target: u16, bytes: &mut [u8; 4]) {
+    const INST_MASK: u32 = ((1 << MOV_IMM16_WIDTH) - 1) << MOV_IMM16_OFFSET;
 
     let mut inst_code = InstructionCode(*bytes).unpack();
     inst_code &= !INST_MASK;
     inst_code |= (target as u32) << MOV_IMM16_OFFSET;
     *bytes = InstructionCode::from_u32(inst_code).0;
-
-    Ok(())
 }
 
-pub fn mov_w_abs_g2s_reloc(value: i64, mem: &mut [u8], offset: usize) -> Result<(), Rel64Error> {
+fn patch_mov_signed(target: i16, bytes: &mut [u8; 4]) {
     const INST_MASK: u32 = (((1 << MOV_IMM16_WIDTH) - 1) << MOV_IMM16_OFFSET)
         | (((1 << MOV_OPCODE_WIDTH) - 1) << MOV_OPCODE_OFFSET);
-
-    let target: i16 = (value >> 32).try_into()?;
-    let bytes = get_bytes_mut(mem, offset)?;
 
     let mut inst_code = InstructionCode(*bytes).unpack();
     inst_code &= !INST_MASK;
@@ -159,20 +109,4 @@ pub fn mov_w_abs_g2s_reloc(value: i64, mem: &mut [u8], offset: usize) -> Result<
         inst_code |= MOV_OPCODE_MOVZ << MOV_OPCODE_OFFSET;
     }
     *bytes = InstructionCode::from_u32(inst_code).0;
-
-    Ok(())
-}
-
-pub fn mov_w_abs_g3_reloc(value: u64, mem: &mut [u8], offset: usize) -> Result<(), Rel64Error> {
-    const INST_MASK: u32 = ((1 << MOV_IMM16_WIDTH) - 1) << MOV_IMM16_OFFSET;
-
-    let target = (value >> 48) as u32;
-    let bytes = get_bytes_mut(mem, offset)?;
-
-    let mut inst_code = InstructionCode(*bytes).unpack();
-    inst_code &= !INST_MASK;
-    inst_code |= target << MOV_IMM16_OFFSET;
-    *bytes = InstructionCode::from_u32(inst_code).0;
-
-    Ok(())
 }
